6dbd2329f5520c94de56de7a65a288c97b145389
wkolowski <wkolowski@protonmail.com>
2020-10-09 23:19:56 +0200

    Figured out the correct disk layout for my main laptop.

--- configuration.nix
+++ configuration.nix
@@ -24,20 +24,15 @@ in
       ./hardware-configuration.nix
     ];
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  # Use the systemd-boot bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   boot.initrd.luks.devices =
   {
     root =
     {
-      device = "/dev/sda2";
+      device = "/dev/sda3";
       preLVM = true;
     };
   };
