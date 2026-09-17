846a86497b98420e6289044da3e5bec5bec86ae5
wkolowski <wkolowski@protonmail.com>
2026-09-17 23:59:11 +0200

    Fix issues with cursor size in Steam.

--- gaming.nix
+++ gaming.nix
@@ -4,8 +4,21 @@
   # 32-bit graphics support needed by Windows games/Wine.
   hardware.graphics.enable32Bit = true;
 
+  nixpkgs.config.allowUnfree = true;
+
+  programs.steam =
+  {
+    enable = true;
+
+    extraPackages = with pkgs;
+    [
+      adwaita-icon-theme
+    ];
+  };
+
   environment.systemPackages = with pkgs;
   [
     lutris
+    protonup-qt
   ];
 }
